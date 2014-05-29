43114553d95097e6fbe04516598c03b77d6b00dc
Ben Kutil <ben@make-things.com>
2014-05-29 12:07:35 -0400

    add deploy details for dev

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,20 @@
+# Project Details
+set :project_id, "p0270"
+set :application, "identity.jhu.edu"
+set :repository,  "git@bitbucket.org:benkutil/p0270-jhu-identity-site.git"
+
 # Set Stages
 set :stages, %w(dev staging production)
 
-set :application, "identity.jhu.edu"
-set :repository,  "set your repository location here"
+# Deploy Details
+set :deploy_via, :remote_cache
+set :copy_cache, true
+set :copy_exclude, ['.git']
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# SSH
+set :ssh_options, {:forward_agent => true}
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+#Global Config
+default_run_options[:pty] = true
+set :keep_releases, 3
+set :use_sudo, false
--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,2 +1,22 @@
-# DEV-specific deployment configuration
-# please put general deployment config in config/deploy.rb
+set :rails_env, :dev
+
+set :branch, fetch(:branch, "dist")
+set :env, fetch(:env, :rails_env)
+
+set :domain, 'ftp.make-things.com'
+set :user, 'makethingsstudio'
+set :port, 22
+
+set :default_server, domain
+set :dest_server, ENV['SERVER'] || default_server
+
+role  :app, dest_server
+role  :web, dest_server
+role  :db,  dest_server, :primary => true
+
+set :keep_releases, 2
+
+set(:deploy_to) {"/home/makethingsstudio/hosts/clients.make-things.com/#{project_id}/#{rails_env}/"}
+
+
+#after 'deploy:create_symlink', 'app:robots'
